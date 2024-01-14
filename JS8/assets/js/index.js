ad217fdfd46778b68b2d86e77be474d90513d9b6
Hòa Bình Vũ <89285687+VuHoaBinh@users.noreply.github.com>
2024-01-14 17:10:16 +0700

    Add files via upload

---
+++ JS8/assets/js/index.js
@@ -0,0 +1,26 @@
+const API_KEY = "4269652c5e1b6a66a34f9d3d16ecc098";
+const DEFAULT_VALUE = "--";
+const searchInput = document.getElementById("search-input");
+const cityName = document.querySelector(".city-name");
+const weatherState = document.querySelector(".weather-state");
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".temperature");
+const sunrise = document.querySelector(".sunrise");
+const sunset = document.querySelector(".sunset");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".wind-speed");
+
+searchInput.addEventListener("change", (event) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${API_KEY}&lang=vi&units=metric`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      cityName.innerHTML = data.name || DEFAULT_VALUE;
+      weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+      weatherIcon.src =
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` ||
+        DEFAULT_VALUE;
+      temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+    });
+});
